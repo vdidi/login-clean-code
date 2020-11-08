6e0683f0ab87fcecee9fbfde6d89cfda13e2fd0b
vdidi <vitor_hugo_di@hotmail.com>
2020-11-08 09:15:08 -0300

    refactor: change api url factory to receive path by param

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,3 +1,3 @@
-export const makeApiUrl = (): string => {
-  return 'http://fordevs.herokuapp.com/login'
+export const makeApiUrl = (path: string): string => {
+  return `http://fordevs.herokuapp.com/api${path}`
 }
