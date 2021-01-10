710526af9248d9a772a875ddf7e78959f6a80019
vdidi <vitor_hugo_di@hotmail.com>
2021-01-10 16:49:44 -0300

    test: ensure SignUp loads with correct state

--- src/main/factories/pages/singup/signup-validation-factory.ts
+++ src/main/factories/pages/singup/signup-validation-factory.ts
@@ -1,11 +1,8 @@
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+import { ValidationComposite, ValidationBuilder as Builder } from '@/validation/validators'
 
-export const makeSignUpValidation = (): ValidationComposite => {
-  return ValidationComposite.build([
-    ...Builder.field('name').required().min(5).build(),
-    ...Builder.field('email').required().email().build(),
-    ...Builder.field('password').required().min(5).build(),
-    ...Builder.field('passwordConfirmation').required().sameAs('password').build()
-  ])
-}
+export const makeSignUpValidation = (): ValidationComposite => ValidationComposite.build([
+  ...Builder.field('name').required().min(5).build(),
+  ...Builder.field('email').required().email().build(),
+  ...Builder.field('password').required().min(5).build(),
+  ...Builder.field('passwordConfirmation').required().sameAs('password').build()
+])
