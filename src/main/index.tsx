218bd37f8cc3d58a5ebd74cf23124873b0fa6cc0
vdidi <vitor_hugo_di@hotmail.com>
2021-02-15 17:17:30 -0300

    refactor: move Route to Main Layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
-import { makeLogin } from './factories/pages/login/login-factory'
-import { makeSignUp } from './factories/pages/singup/signup-factory'
+import Router from '@/main/routes/router'
 
 ReactDOM.render(
-  <Router
-    makeLogin={makeLogin}
-    makeSignUp={makeSignUp}
-  />,
+  <Router />,
   document.getElementById('main')
 )
