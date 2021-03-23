a1ca5ce0ab8697409eaa679abd6fed2c41a6f55a
vdidi <vitor_hugo_di@hotmail.com>
2021-03-23 20:58:08 -0300

    feat: ensure SurveyList render SurveyItem on success

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,7 +1,8 @@
+import { SurveyModel } from '@/domain/models'
 import { LoadSurveyList } from '@/domain/usecases'
 import { Footer, Header } from '@/presentation/components'
-import React, { useEffect } from 'react'
-import { SurveyItemEmpty } from './components'
+import React, { useEffect, useState } from 'react'
+import { SurveyItem, SurveyItemEmpty } from './components'
 import Styles from './survey-list-styles.scss'
 
 type Props = {
@@ -9,10 +10,13 @@ type Props = {
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[]
+  })
+
   useEffect(() => {
-    (async function () {
-      loadSurveyList.loadAll()
-    })()
+    loadSurveyList.loadAll()
+      .then(surveys => setState({ surveys }))
   }, [])
 
   return (
@@ -21,7 +25,10 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul data-testid='survey-list'>
-          <SurveyItemEmpty />
+          {state.surveys.length
+            ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
+            : <SurveyItemEmpty />
+          }
         </ul>
       </div>
       <Footer />
